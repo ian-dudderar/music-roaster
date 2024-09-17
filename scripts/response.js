6119e5e6eded0a1a03b8e0fd3a0d60bb0fe82d78
ian-dudderar <dudderari@gmail.com>
2024-09-17 16:18:41 -0400

    Save

--- scripts/response.js
+++ scripts/response.js
@@ -1,8 +1,10 @@
 var playlists = [];
+const fakeres = ["Hac feugiat cubilia curae aliquam vehicula diam. Primis nec enimcommodo sapien sagittis fermentum magna. Fermentum vulputate velitturpis pharetra cras euismod. Eget metus pharetra cursus pretium duis venenatis. Senectus vel hendrerit felis, himenaeos mollis finibus. Dignissim porttitor ridiculus ligula tellus ante morbi id elementum primis. Dolor proin mi hendrerit ultricies in felis. Feugiat cras odio tristique; rutrum taciti parturient quis hac. Viverra auctor rhoncus metus; imperdiet diam dui taciti? Lacus phasellus pellentesque tempor scelerisque vestibulum nascetur tincidunt?"]
 async function initializePage() {
     document.getElementById("image").src =
         sessionStorage.getItem("playlistPhoto");
     loadStars();
+    textDelay(fakeres)
 }
 
 function loadStars() {
@@ -41,6 +43,60 @@ function loadStars() {
 
     fetchRandomNumber().then((count) => {
         activateStars(5);
+    });
+}
+
+async function textDelay(text_array) {
+    console.log("textdelay");
+    let i = 1;
+    let useCursor = true;
+    for (let text of text_array) {
+        if (i != text_array.length) {
+            text = text + "..";
+        } else {
+            useCursor = false;
+        }
+        //Creates the element for the response text and passes them both to the typewriter effect
+        const paragraph = document.createElement("p");
+        document.querySelector(".rating-text").appendChild(paragraph);
+
+        //Creates a blinking cursor element, then deletes it before the next cycle
+        let cursor = await textTypewriterEffect(paragraph, text, useCursor);
+        await sleep(2000);
+        if (useCursor) paragraph.removeChild(cursor);
+        i++;
+    }
+}
+
+// Function to simulate typewriter effect
+function textTypewriterEffect(element, text, useCursor, i = 0) {
+    return new Promise((resolve) => {
+        element.textContent += text[i];
+        element.scrollIntoView();
+        if (i === text.length - 1) {
+            // Create a blinking cursor element and appends it
+            const cursor = document.createElement("span");
+            cursor.classList.add("cursor");
+            cursor.innerHTML = "|";
+            if (useCursor) element.appendChild(cursor);
+            // element.appendChild(cursor);
+            return resolve(cursor);
+        } else {
+            //Slightly randomizes the speed of the typewriter
+            // return setTimeout(
+            //   () =>
+            //     resolve(textTypewriterEffect(element, text, useCursor, i + 1)),
+            //   Math.floor(Math.random() * (30 - 0 + 1) + 0)
+            // );
+            return setTimeout(
+                () =>
+                    resolve(textTypewriterEffect(element, text, useCursor, i + 1)),
+                Math.floor(Math.random() * (70 - 40 + 1) + 40)
+            );
+            // return resolve(
+            //   textTypewriterEffect(element, text, useCursor, i + 1)
+            // );
+        }
     });
 }
 
